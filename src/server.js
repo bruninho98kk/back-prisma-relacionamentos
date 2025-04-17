cd01c450d75a205d65a14d1e3daaf1e3ca995a27
bruninho98kk <158165665+bruninho98kk@users.noreply.github.com>
2025-04-17 08:01:01 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { config } from "dotenv";
+
+import animesRouter from "./routes/animeRoutes.js";
+import personagensRouter from "./routes/personagemRoutes.js";
+
+config(); // Carrega variáveis de ambiente do arquivo .env
+const port = process.env.PORT || 4000;
+
+// Inicializa o Express
+const app = express();
+
+app.use(express.json()); // Parse de JSON
+
+app.use("/animes", animesRouter); // Usar as rotas de animes
+app.use("/personagens", personagensRouter); // Usar as rotas de personagens
+
+// Rota base para verificar se o servidor está rodando
+app.get("/", (req, res) => {
+  res.json({ message: "API de Coleção de Animes funcionando!" });
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
